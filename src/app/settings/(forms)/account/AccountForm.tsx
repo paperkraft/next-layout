b21eccf808c0079c31a2c10280d3d6115815fd25
test <test@github.com>
2024-09-19 12:48:26 +0530

    w

--- src/app/settings/(forms)/account/AccountForm.tsx
+++ src/app/settings/(forms)/account/AccountForm.tsx
@@ -57,7 +57,7 @@ export function AccountForm() {
   function onSubmit(data: AccountFormValues) {
     toast("You submitted the following values", {
       description: (
-        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+        <pre className="mt-2 w-[340px] md:w-[320px] rounded-md bg-slate-950 p-4">
           <code className="text-white">{JSON.stringify(data, null, 2)}</code>
         </pre>
       )
